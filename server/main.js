825cbf5488905a632abfafdbf6249d0a1a64de19
Juan David Velasquez <jdvr1994@hotmail.com>
2018-01-29 19:51:12 -0500

    Creacion WebSocket.js

--- server/main.js
+++ server/main.js
@@ -1,48 +1,19 @@
 
-const app = require('./app');
-const config = require('../config');
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+'use strict'
 
-const mongoose = require('mongoose');
-
-const Photon = require('../controllers/photon');
-
-var messages = [{
-        id : 1,
-        text : "Hola soy el servidor mas pro",
-        author : "Servidor"
-}];
+const app = require('./app')
+const server = require('./webSocket')
+const config = require('../config')
+const mongoose = require('mongoose')
 
 mongoose.connect(config.db,function(err, res){
     if(err) {
       return console.log(`Error al conectar a la base de datos: ${err}`);
     }
-    console.log('Conexion a la base de datos establecida...');
-});
+    console.log('Conexion a la base de datos establecida...')
 
-//---------- Web Socket -----------------------
-//---------------------------------------------
-io.on('connection', function(socket) {
-  console.log('Alguien se ha conectado con Sockets');
-  socket.emit('messages', messages);
+    server.listen(config.portServer, function() {
+      console.log("Servidor corriendo en http://localhost:"+config.portServer);
+    });
 
-  socket.on('disconnect', function(data) {
-    console.log("Cliente desconectado");
-  });
-
-  socket.on('new-message', function(data) {
-      messages.push(data);
-      io.sockets.emit('messages', messages);
-  });
-
-  socket.on('sound-change', function(data){
-	   var driver = JSON.parse(data);
-     Photon.setModeVumeter(driver.sonido.boton);
-	   console.log("android OK"+driver.sonido.boton);
-  });
-});
-
-server.listen(config.portServer, function() {
-  console.log("Servidor corriendo en http://localhost:"+config.portServer);
-});
+})
---
+++ server/webSocket.js
@@ -0,0 +1,36 @@
+'use strict'
+const app = require('./app');
+const config = require('../config');
+const Photon = require('../controllers/photon');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+var messages = [{
+        id : 1,
+        text : "Hola soy el servidor mas pro",
+        author : "Servidor"
+}];
+
+//---------- Web Socket -----------------------
+//---------------------------------------------
+io.on('connection', function(socket) {
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
+
+  socket.on('disconnect', function(data) {
+    console.log("Cliente desconectado");
+  });
+
+  socket.on('new-message', function(data) {
+      messages.push(data);
+      io.sockets.emit('messages', messages);
+  });
+
+  socket.on('sound-change', function(data){
+	   var driver = JSON.parse(data);
+     Photon.setModeVumeter(driver.sonido.boton);
+	   console.log("android OK"+driver.sonido.boton);
+  });
+});
+
+module.exports = server
