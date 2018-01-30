c092cadf6e8445903694d674f0eb3979dfa43068
Juan David Velasquez <jdvr1994@hotmail.com>
2018-01-30 09:45:39 -0500

    Controller Chat V2.0

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,4 +1,9 @@
 'use strict'
+var messages = [{
+        id : 1,
+        text : "Hola soy el servidor mas pro",
+        author : "Servidor"
+}];
 
 function begin(socket){
   socket.emit('messages', messages);
