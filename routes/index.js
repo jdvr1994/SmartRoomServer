0e7fc0951d2adae4caceb756515a2dceef47c3aa
Juan David Velasquez <jdvr1994@hotmail.com>
2018-01-30 20:47:01 -0500

    API POST para signUp y singIn

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const ProductCrtl = require('../controllers/product');
 const auth = require('../middlewares/auth')
+const UserCtrl = require('..controllers/user')
 const api = express.Router()
 
 api.get('/product', ProductCrtl.getProducts);
@@ -14,5 +15,8 @@ api.delete('/product/:productId',ProductCrtl.deleteProduct);
 api.get('/private',auth,(req,res)=>{
   res.status(200).send({mensaje: 'Tienes acceso'})
 });
+
+api.post('/signup',UserCtrl.signup);
+api.post('/signin',UserCtrl.signin);
 
 module.exports = api
