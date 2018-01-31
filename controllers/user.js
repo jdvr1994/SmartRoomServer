ed5e6583040568198a4ec8710bc756ad2ecf9f31
Juan David Velasquez <jdvr1994@hotmail.com>
2018-01-30 22:05:10 -0500

    SignUp y SignIn User OK OK OK

--- controllers/user.js
+++ controllers/user.js
@@ -12,7 +12,6 @@ function signUp (req, res) {
 
   user.save((err) => {
     if (err) return res.status(500).send({ message: `Error al crear el usuario: ${err}` })
-
     return res.status(201).send({ token: service.createToken(user) })
   })
 }
@@ -21,9 +20,6 @@ function signIn (req, res) {
   User.find({ email: req.body.email }, (err, user) => {
     if (err)return res.status(500).send({ message: err })
     if (!user || user.length==0) return res.status(404).send({ message: 'No existe el usuario' })
-
-    console.log(req.body)
-    console.log(user)
 
     req.user = user
     res.status(200).send({
