a18512498479da87f0f6f0d8a624d69b0b3b5eb9
Juan David Velasquez <jdvr1994@hotmail.com>
2018-01-30 21:44:27 -0500

    asdasd

--- controllers/user.js
+++ controllers/user.js
@@ -19,16 +19,11 @@ function signUp (req, res) {
 
 function signIn (req, res) {
   User.find({ email: req.body.email }, (err, user) => {
-    if (err){
-      return res.status(500).send({ message: err })
-    }
-
-    if (!user) {
-      return res.status(404).send({ message: 'No existe el usuario' })
-      console.log('No existe el usuario')
-    }
+    if (err)return res.status(500).send({ message: err })
+    if (!user) return res.status(404).send({ message: 'No existe el usuario' })
 
     console.log(req.body)
+    console.log(user)
 
     req.user = user
     res.status(200).send({
