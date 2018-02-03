a10e91a3c83082d1602e6114e1f93f320d89b0d5
Juan David Velasquez <jdvr1994@hotmail.com>
2018-02-02 22:11:23 -0500

    Closet API V1.0.7

--- controllers/closet.js
+++ controllers/closet.js
@@ -20,7 +20,7 @@ function signIn (driver) {
   Closet.find({ driverId: driver.driverId }, (err, closets) => {
     if (err)return console.log({ message: err })
     if (!closets || closets.length==0) return console.log({ message: 'No existe el usuario' })
-    closet.comparePass(driver.password,closets[0].password)
+    closets[0].comparePass(driver.password,closets[0].password)
 
     console.log({
       message: 'Te has logueado correctamente',
